08433bbaf5143856c331785f96a065fbd1403f86
LMBorderon <borderon.lm@gmail.com>
2024-06-04 15:46:06 +0200

    maj api - ajouter / lire / maj / supp utilisateur

--- api.py
+++ api.py
@@ -84,7 +84,50 @@ def article():
 def commande():
     return
 
-# AFFICHER INFOS GENERALES UTILISATEUR ( PAS ID ET ROLE )
+# Ajouter Utilisateur :
+
+@app.route('/utilisateurs', methods=['GET'])
+def ajout_utilisateur():
+    data = request.json
+    nouveau_utilisateur = Utilisateur(
+        code_utilisateur=data["code_utilisateur"],
+        nom_utilisateur=data["nom_utilisateur"],
+        prenom_utilisateur=data["prenom_utilisateur"],
+        username=data["username"],
+        couleur_fond_utilisateur=data["couleur_fond_utilisateur"],
+        date_insc_utilisateur=data["date_insc_utilisateur"]
+    )
+
+    session.add(nouveau_utilisateur)
+    session.commit()  
+    session.refresh(nouveau_utilisateur)
+    return jsonify({
+        "code_utilisateur": nouveau_utilisateur.code_utilisateur,
+        "nom_utilisateur": nouveau_utilisateur.nom_utilisateur,
+        "prenom_utilisateur": nouveau_utilisateur.prenom_utilisateur,
+        "username": nouveau_utilisateur.username,
+        "couleur_fond_utilisateur": nouveau_utilisateur.couleur_fond_utilisateur,
+        " date_insc_utilisateur": nouveau_utilisateur.date_insc_utilisateur
+    }), 201
+
+    if __name == "__main":
+        app.run(debug=True)
+
+# Afficher liste utilisateur
+
+@app.route("/utilisateurs", methods=["GET"])
+def read_liste_utilisateurs():
+    utilisateurs = session.query(Utilisateur).all()
+    return jsonify([{
+            "code_utilisateur": utilisateur.code_utilisateur,
+            "nom_utilisateur": utilisateur.nom_utilisateur,
+            "prenom_utilisateur": utilisateur.prenom_utilisateur,
+            "username": utilisateur.username,
+            "couleur_fond_utilisateur": utilisateur.couleur_fond_utilisateur,
+            "date_insc_utilisateur": utilisateur.date_insc_utilisateur
+    } for utilisateur in utilisateurs])
+
+# Afficher infos utilisateur
 
 @app.route('/utilisateurs/<int:code_utilisateur>', methods=['GET'])
 def read_utilisateur(code_utilisateur):
@@ -101,6 +144,79 @@ def read_utilisateur(code_utilisateur):
             "date_insc_utilisateur": utilisateur.date_insc_utilisateur
     }])
  
+# Mettre à jour les infos Utilisateur 
+
+@app.route("/utilisateurs/<int:code_utilisateur>", methods=["PUT"])
+def maj_utilisateur(code_utilisateur):
+    data = request.json
+    utilisateur = session.query(Utilisateur).filter(Utilisateur.code_utilisateur == code_utilisateur).first()
+
+    if utilisateur is None:
+        return jsonify({"message": "Utilisateur non trouvé"}), 404
+
+    else:
+        if "code_utilisateur" in data:
+            utilisateur.code_utilisateur = data["code_utilisateur"]
+        if "nom_utilisateur" in data:
+            utilisateur.nom_utilisateur = data["nom_utilisateur"]
+        if "prenom_utilisateur" in data:
+            utilisateur.prenom_utilisateur = data["prenom_utilisateur"]
+        if "username" in data:
+            utilisateur.username = data["username"]
+        if "couleur_fond_utilisateur" in data:
+            utilisateur.couleur_fond_utilisateur = data["couleur_fond_utilisateur"]
+        if "date_insc_utilisateur" in data:
+            utilisateur.date_insc_utilisateur = data["date_insc_utilisateur"]
+
+    session.commit()
+    session.refresh(utilisateur)
+
+    return jsonify({
+        "code_utilisateur":  utilisateur.code_utilisateur,
+        "nom_utilisateur":  utilisateur.nom_utilisateur,
+        "prenom_utilisateur":  utilisateur.prenom_utilisateur,
+        "username":  utilisateur.username,
+        "couleur_fond_utilisateur":  utilisateur.couleur_fond_utilisateur,
+        "date_insc_utilisateur":  utilisateur.date_insc_utilisateur
+    }) 
+
+# Supprimer Utilisateur 
+
+@app.route("/utilisateurs/<int:code_utilisateur>", methods=["PUT"])
+def maj_utilisateur(code_utilisateur):
+    data = request.json
+    utilisateur = session.query(Utilisateur).filter(Utilisateur.code_utilisateur == code_utilisateur).first()
+
+    if utilisateur is None:
+        return jsonify({"message": "Utilisateur non trouvé"}), 404
+
+    else:
+        if "code_utilisateur" in data:
+            utilisateur.code_utilisateur = data["code_utilisateur"]
+        if "nom_utilisateur" in data:
+            utilisateur.nom_utilisateur = data["nom_utilisateur"]
+        if "prenom_utilisateur" in data:
+            utilisateur.prenom_utilisateur = data["prenom_utilisateur"]
+        if "username" in data:
+            utilisateur.username = data["username"]
+        if "couleur_fond_utilisateur" in data:
+            utilisateur.couleur_fond_utilisateur = data["couleur_fond_utilisateur"]
+        if "date_insc_utilisateur" in data:
+            utilisateur.date_insc_utilisateur = data["date_insc_utilisateur"]
+
+    session.commit()
+    session.refresh(utilisateur)
+
+    return jsonify({
+        "code_utilisateur":  utilisateur.code_utilisateur,
+        "nom_utilisateur":  utilisateur.nom_utilisateur,
+        "prenom_utilisateur":  utilisateur.prenom_utilisateur,
+        "username":  utilisateur.username,
+        "couleur_fond_utilisateur":  utilisateur.couleur_fond_utilisateur,
+        "date_insc_utilisateur":  utilisateur.date_insc_utilisateur
+    }) 
+
+
 
 
 app.run()
